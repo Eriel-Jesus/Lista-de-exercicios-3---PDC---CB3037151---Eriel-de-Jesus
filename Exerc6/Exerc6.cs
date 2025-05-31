f73163a961f459221715013b72496f07c4b9be03
Eriel-Jesus <83691312+Eriel-Jesus@users.noreply.github.com>
2025-05-31 20:18:07 -0300

    Add files via upload

---
+++ Exerc6/Exerc6.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exerc6
+{
+    internal class Exerc6
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            int maior, menor;
+            double valor;
+            
+            bool verifica;
+
+            Console.Write("Insira o número de qual será a tabuada: ");
+            verifica = double.TryParse(Console.ReadLine(), out valor);
+
+            while (verifica == false || valor < 0)
+            {
+                Console.Clear();
+                Console.Write("Insira corretamente o número de qual será a tabuada: ");
+                verifica = double.TryParse(Console.ReadLine(), out valor);
+            }
+            Console.Clear();
+
+            Console.Write("Por onde a tabuada deve começar? ");
+            verifica = int.TryParse(Console.ReadLine(), out menor);
+
+            while (verifica == false || menor < 0)
+            {
+                Console.Clear();
+                Console.Write("Insira por onde a tabuada deve começar: ");
+                verifica = int.TryParse(Console.ReadLine(), out menor);
+            }
+            Console.Clear();
+
+            Console.Write("Em qual múltiplo a tabuada deve terminar: ");
+            verifica = int.TryParse(Console.ReadLine(), out maior);
+
+            while (verifica == false || maior < menor)
+            {
+                Console.Clear();
+                Console.Write("Insira em qual múltiplo a tabuada deve encerrar: ");
+                verifica = int.TryParse(Console.ReadLine(), out maior);
+            }
+
+
+            Console.Clear();
+            Console.WriteLine("Tabuada do {0}!", valor);
+            Console.WriteLine();
+            i = menor;
+            do
+            {
+                Console.WriteLine("{0} X {1} = {2}", valor, i, valor * i);
+                i++;
+            } while (i < (maior + 1));
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Exerci10/Exerc10.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exerci10
+{
+    internal class Exerc10
+    {
+        static void Main(string[] args)
+        {
+            int[] valores = new int[10];
+            int maior = 0, soma = 0;
+
+            for (int i = 0; i < 10; i++)
+            {
+                int valor;
+                do
+                {
+                    Console.Write("Digite o "+ (i+1) +" º valor positivo: ");
+                    valor = int.Parse(Console.ReadLine());
+                    if (valor <= 0)
+                        Console.WriteLine("Valor inválido. Digite novamente: ");
+                } while (valor <= 0);
+
+                valores[i] = valor;
+                soma += valor;
+                if (valor > maior)
+                    maior = valor;
+            }
+
+            double media = soma / 6.0;
+            Console.WriteLine("Maior valor: " + maior);
+            Console.WriteLine("Soma dos valores: " + soma);
+            Console.WriteLine("Média aritmética: " + media.ToString("F2"));
+        }
+    }
+}
